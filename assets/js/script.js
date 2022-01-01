10a65a3651c7b58f958ec1f6d0fe1a224171b2be
TCL git <tuckerlarrabee13@gmail.com>
2022-01-01 18:29:20 -0500

    moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,15 +82,23 @@ $(".list-group").on("click", "span", function(){
   //create new input element
   var dateInput = $("<input>").attr("type", "text").addClass("form-control").val(date);
 
-  //swap out elements
   $(this).replaceWith(dateInput);
+
+  //enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      //when calendar is closed, force a "change" event on the dateInput
+      $(this).trigger("change");
+    }
+  })
 
   // automatically focus on new element
   dateInput.trigger("focus");
 });
 
 //value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   //get current text
   var date = $(this).val().trim();
 
@@ -216,4 +224,8 @@ $("#trash").droppable({
   out: function(event, ui) {
     console.log("out")
   }
+});
+
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
