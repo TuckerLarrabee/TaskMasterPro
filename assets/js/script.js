6f83f1133c538eab9cd92958bb382b14b7dd0033
TCL git <tuckerlarrabee13@gmail.com>
2022-01-02 10:45:22 -0500

    added bootstrap background colors to tasks based on due date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 var tasks = {};
 
+var auditTask = function (taskEl) {
+  //get date from task element
+  var date = $(taskEl).find("span").text().trim();
+  //convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  //remove old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+  //apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger")
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+}
+
 var createTask = function (taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -8,6 +23,9 @@ var createTask = function (taskText, taskDate, taskList) {
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
+
+  //check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -105,10 +123,8 @@ $(".list-group").on("change", "input[type='text']", function() {
   //get the parent ul's id attribute
   var status = $(this).closest(".list-group").attr("id").replace("list-", "");
   
-  
   //get the task's position in the list of other li elements
   var index = $(this).closest(".list-group-item").index();
-  
 
   //update task in array and re-save to localstorage
   tasks[status][index].date = date;
@@ -119,6 +135,9 @@ $(".list-group").on("change", "input[type='text']", function() {
 
   //replace input with span element
   $(this).replaceWith(taskSpan);
+
+  //Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 })
 
 // modal was triggered
@@ -229,3 +248,8 @@ $("#trash").droppable({
 $("#modalDueDate").datepicker({
   minDate: 1
 });
+
+// var auditTask = function (taskEl) {
+//   //to ensure element is getting to the function
+//   console.log(taskEl);
+// }
