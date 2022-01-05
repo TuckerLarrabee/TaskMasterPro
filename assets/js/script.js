2e936e6a0bfbd1bfb115a91664fedc40104031e1
TCL git <tuckerlarrabee13@gmail.com>
2022-01-05 16:38:44 -0500

    save

--- assets/js/script.js
+++ assets/js/script.js
@@ -192,10 +192,10 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    // console.log("activate", this);
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    // console.log("deactivate", this);
   },
   over: function(event) {
     console.log("over", event.target);
